79a2a38e3b3a298679e546181f672e1ef5d3d3bc
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-17 17:24:25 +0000

    fix: make introspection command output more useful (JSON-stringified), some test additions (#36)

---
+++ packages/calculang-js/tests/csv.test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment node
+ */
+
+import compiler from '@calculang/calculang-js';
+
+jest.setTimeout(20000);
+
+test.each(['manufacturing/products'])('%s', async (d) => {
+  const output = await compiler(
+    `./packages/calculang-testcase-models/${d}.cul.js`,
+    {}
+  );
+
+  expect({
+    bundle: output.bundle.replace(/\\r/g, ''),
+    sourcemap: output.sourcemap.replace(/\\r/g, ''), // remove carriage returns
+  }).toMatchSnapshot();
+});
+
+//

dc5499f1e5e01d47b1f7df8eb5b3d08ee2ec1352
Declan Naughton <declan.naughton2@mail.dcu.ie>
2022-02-11 18:10:49 +0000

    feat: "esm" output (replaces 'verbose' output and corrects source references)

--- packages/calculang-js/src/calculang-transform-loader/index.js
+++ packages/calculang-js/src/calculang-transform-loader/index.js
@@ -19,6 +19,7 @@ import { getOptions, parseQuery } from 'loader-utils';
 import { transformSync } from '@babel/core';
 
 import visitor from './visitor';
+import esmvisitor from './esmvisitor';
 
 var chk = 0;
 
@@ -46,12 +47,29 @@ export default function loader(content, map, meta) {
     sourceMaps: true,
   });
 
+  // make transformed_esm = transformed, but remapping import sources using cul_scope_ids_to_resource where there is a match.
+  // is calling this feat esm legit?
+  // if cul_scope_ids_to_resource ignores non-cul, then to extent esm in we have got esm out
+  // => verbose becomes esm.
+
+  // note: not esm because of dependence on webpack e.g. raw-loader!
+
+  const transformed_esm = transformSync(transformed.code, {
+    filename: this.resourcePath, //+ this.resourceQuery, //JSON.stringify(params), // to set sourceFileName, but has no impact
+    inputSourceMap: transformed.map,
+    babelrc: false,
+    generatorOpts: { /*compact: true*/ retainLines: true },
+    configFile: false,
+    plugins: [[esmvisitor, options]],
+    sourceMaps: true,
+  });
+
   // keeping because helpful, but needs to be done smarter, issue #34 filed
   /*console.log(`\n\n\ncul_scope_id = ${options.params.cul_scope_id}`);
   console.log('==================================');
   console.log(transformed.code);*/
   //if (!chk) this.emitFile('verbose/b', transformed.code);
-  this.emitFile(`verbose/${options.params.cul_scope_id}.js`, transformed.code); // fut also replace (link up correctly) import refs here
+  this.emitFile(`esm/${options.params.cul_scope_id}.js`, transformed_esm.code); // fut also replace (link up correctly) import refs here
 
   this.callback(null, transformed.code, transformed.map, meta);
   return; // "always return undefined when calling callback()" https://v4.webpack.js.org/api/loaders/

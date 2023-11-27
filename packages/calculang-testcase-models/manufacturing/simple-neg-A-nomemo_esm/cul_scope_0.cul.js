c6e0007e6f401f9c07ebefc6f2b9ca496a6521d7
Declan Naughton <declann1990@gmail.com>
2023-11-27 16:59:53 +0000

    remove nomemos

--- packages/calculang-testcase-models/manufacturing/simple-neg-A-nomemo_esm/cul_scope_0.cul.js
+++
@@ -1,7 +0,0 @@
-// 'neg' logic is the most delicate of all the logic in calculang
-// I had more testcases in development, and need to add more
-
-export const sales_by_product = () => (product() == 1 ? 100 : 50);
-export const product = () => product_in;
-export const sales_total = () =>
-  sales_by_product({ product_in: 0 }) + sales_by_product({}); // => sales_total NOT independent of product_in (this is coded to add some products sales to product 0 sales : bad, but legit from calculang perspective)
--- packages/calculang-testcase-models/manufacturing/simple-neg-A-nomemo_esm/cul_scope_0.mjs
+++
@@ -1,7 +0,0 @@
-// 'neg' logic is the most delicate of all the logic in calculang
-// I had more testcases in development, and need to add more
-
-export const sales_by_product = ({ product_in }) => product({ product_in }) == 1 ? 100 : 50;
-export const product = ({ product_in }) => product_in;
-export const sales_total = ({ product_in }) =>
-sales_by_product({ product_in: 0 }) + sales_by_product({ product_in }); // => sales_total NOT independent of product_in (this is coded to add some products sales to product 0 sales : bad, but legit from calculang perspective)

d3ec1dd5dbbd5f6b8fa11f77c97b1fb5722088de
Declan Naughton <declann1990@gmail.com>
2024-12-18 11:10:14 +0000

    delete sum test

--- packages/standalone/test/sum.js
+++
@@ -1,4 +0,0 @@
-// sum.js
-export function sum(a, b) {
-  return a + b
-}
--- packages/standalone/test/sum.test.js
+++
@@ -1,7 +0,0 @@
-// sum.test.js
-import { expect, test } from 'vitest'
-import { sum } from './sum.js'
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})

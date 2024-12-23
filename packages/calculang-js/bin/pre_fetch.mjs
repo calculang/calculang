191317e7e0925deeb454b71253bc13d760f755bd
Declan Naughton <declann1990@gmail.com>
2024-12-23 01:01:48 +0000

    force abs paths and fix tests

--- packages/calculang-js/bin/pre_fetch.mjs
+++ packages/calculang-js/bin/pre_fetch.mjs
@@ -4,7 +4,7 @@ import {readFile} from 'node:fs/promises'
 
 import * as Babel from '../../standalone/babel.mjs' // but I can conditionally use node api - prob a bad idea?
 
-// returns fs
+// returns fs. fs uses global references except for entrypoint and updates code to use global references. This is essential because e.g. ./base.cul.js can refer to different files depending on file/model structure
 export async function pre_fetch(entrypoint) {
 
   let next = []; // next imports to traverse
@@ -12,13 +12,12 @@ export async function pre_fetch(entrypoint) {
   let fs = {}
 
   async function pre_fetch_(entrypoint, resolved) {
-    fs[entrypoint] = (await readFile(resolve(resolved), 'utf8'))//.toString('ascii')
+    let start = (await readFile(resolve(resolved), 'utf8'))//.toString('ascii')
 
-    let code = fs[entrypoint]
     let dirname_parent = dirname(resolved)
     
     // CHOICES to make graph for all_cul replacement collection, make graph directly in visior or make it from cul_scope_ids_to_resource
-    Babel.transform(code, {
+    fs[resolved] = Babel.transform(start, {
       plugins: [
         [({ types }) =>
         ({
@@ -26,14 +25,17 @@ export async function pre_fetch(entrypoint) {
           visitor: {
             ImportDeclaration(path) {
               if (!path.node.source.value.includes('.cul')) return;
+              
+              const resolved = resolve(dirname_parent, path.node.source.value)
+              path.node.source.value = resolved
 
-              next.push({resource: path.node.source.value, resolved: resolve(dirname_parent, path.node.source.value)}) // this seems to work TODO remote URLs
+              next.push({resource: path.node.source.value, resolved}) // this seems to work TODO remote URLs
             },
 
           }
         })
         ]]
-    });
+    }).code;
 
     for (const n of next) {
       if (!fs.hasOwnProperty(n.resource))
